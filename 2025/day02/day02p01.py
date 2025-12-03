f5ae8c750e3512216ab046d91d16a2fb92780adf
sublimeS0 <sublime@lemonsharksoftware.com>
2025-12-02 23:04:50 -0500

    Day 2 complete

---
+++ 2025/day02/day02p01.py
@@ -0,0 +1,46 @@
+def main():
+    """
+    Entry point for Day 2, Part 1
+
+    18952700150 - correct
+
+    :return: Exit code
+    """
+
+    ranges = read_input('input.txt')
+
+    invalid_sum = 0
+    for range_item in ranges:
+        for value in range(range_item['lower'], range_item['upper'] + 1):
+            first_half, second_half = str(value)[:len(str(value))//2], str(value)[len(str(value))//2:]
+
+            if first_half == second_half:
+                invalid_sum += value
+
+    print('Invalid Sum: ' + str(invalid_sum))
+
+
+def read_input(filename):
+    """
+    Read input and covert to relevant data structure
+    :param filename: Name of input file
+    :return: List of dictionaries containing range info
+    """
+    with open(filename) as file:
+        for line in file:
+            ranges = line.split(',')
+
+    split_ranges = []
+    for item in ranges:
+        values = item.split('-')
+
+        split_ranges.append({
+            'lower': int(values[0]),
+            'upper': int(values[1]),
+        })
+
+    return split_ranges
+
+
+if __name__ == "__main__":
+    main()
